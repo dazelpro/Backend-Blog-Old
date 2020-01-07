7abee9e87b5419cc4e5f98472f11c6901a347f72
Zeldianto Eka Putra <dazelpro@gmail.com>
2020-01-07 19:14:29 +0700

    Deploy-10

--- app-config/database.js
+++ app-config/database.js
@@ -6,6 +6,34 @@ const mysql         = require('mysql');
 //     password            : '',
 //     database            : 'db_go_blog'
 // });
+
+// var conn;
+// function handleDisconnect() {
+//     conn = mysql.createConnection({
+//         host: 'remotemysql.com',
+//         user: 'MmAYJmLRyJ',
+//         password: 'i4bArzUxlo',
+//         database: 'MmAYJmLRyJ'
+//     });
+
+//     conn.connect(function(err) {    
+//         console.log('Connect To database...');
+//         if(err) {
+//         console.log('error when connecting to db:', err);
+//         setTimeout(handleDisconnect, 2000); 
+//         }                               
+//     });                                   
+
+//     conn.on('error', function(err) {
+//         console.log('db error', err);
+//         if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//         handleDisconnect();                         
+//         } else {                                      
+//         throw err;                                  
+//         }
+//     });
+// }
+
 const connection    = mysql.createConnection({
     multipleStatements  : true,
     host                : 'remotemysql.com',
@@ -15,8 +43,11 @@ const connection    = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log('Connect To database...');
+    console.log('Connect to database...');
+    if(err) {
+        console.log('error when connecting to db:', err);
+        setTimeout(connection, 1000); 
+    }
 });
 
 module.exports = connection;
--- app-controllers/controller-users.js
+++ app-controllers/controller-users.js
@@ -1,8 +1,9 @@
 const database      = require('../app-config/database');
-const slug          = require('slug');
+const md5           = require('md5');
+
 module.exports ={
     userAll(req,res){
-        database.query('SELECT * FROM tbl_user', function (error, results) {
+        database.query('SELECT user_id, user_name, user_email FROM tbl_user', function (error, results) {
         if(error)
             return res.status(400).send({
                 success: false,
@@ -15,11 +16,13 @@ module.exports ={
         }); 
     },
     userAdd(req,res){        
-        let name    = req.body.user_name;
-        let slugs   = slug(req.body.user_name, {lower: true});
+        let user_name    = req.body.user_name;
+        let user_email   = req.body.user_email;
+        let user_password= md5(req.body.user_pass);
         database.query("INSERT INTO tbl_user SET ? ", { 
-            user_name: name, 
-            user_slug: slugs,
+            user_name: user_name,
+            user_email: user_email,
+            user_password: user_password,
         }, function (error, results) {
         if(error)
             return res.status(400).send({
